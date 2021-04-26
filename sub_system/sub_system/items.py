c501bd64e41946c5437a92ce4979f45384fdd31d
PtCu <1365699009@qq.com>
2021-04-27 00:31:44 +0800

    trial

--- sub_system/sub_system/items.py
+++ sub_system/sub_system/items.py
@@ -10,3 +10,15 @@ class SubSystemItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class DeviceItem(scrapy.Item):
+    name=scrapy.Field() # 监测站名名称
+    id=scrapy.Field() # 设备编号
+    phone=scrapy.Field() #手机卡号
+    type=scrapy.Field() # 监测站类型
+    model=scrapy.Field() #径流量计算模型
+    location=scrapy.Field() #设备位置
+    photo=scrapy.Field() #现场照片
+    belong_to=scrapy.Field() #所属监测站
+
+
